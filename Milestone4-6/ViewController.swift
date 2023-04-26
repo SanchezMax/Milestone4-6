7ab1019f440ef10c81264f6076639e59d2b4bdb2
Max Zykin <sanchez.max@yandex.ru>
2023-04-26 13:36:32 +0400

    Initial Commit

---
+++ Milestone4-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone4-6
+//
+//  Created by Aleksey Novikov on 26.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
